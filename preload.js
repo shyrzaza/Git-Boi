8e190f6fe90a72fed37d8cde80dc8a73f9f7dce8
Cedric Fromm <cedric@yayap.de>
2025-10-18 22:36:56 +0200

    v1.3.0
    vibecode some refactoring

--- preload.js
+++ preload.js
@@ -1,9 +1,45 @@
+/**
+ * Git-Boi Preload Script
+ * Provides window management functionality to the renderer process
+ */
+
 const { remote } = require('electron');
 
-let currWindow = remote.BrowserWindow.getFocusedWindow();
+// Get reference to the current window
+const currentWindow = remote.BrowserWindow.getFocusedWindow();
 
-window.closeCurrentWindow = () => {
-    if (currWindow) {
-        currWindow.close();
+/**
+ * Expose window management functions to renderer process
+ */
+window.windowManager = {
+    /**
+     * Safely close the current window
+     */
+    closeWindow: () => {
+        if (currentWindow && !currentWindow.isDestroyed()) {
+            currentWindow.close();
+        }
+    },
+
+    /**
+     * Minimize the current window
+     */
+    minimizeWindow: () => {
+        if (currentWindow && !currentWindow.isDestroyed()) {
+            currentWindow.minimize();
+        }
+    },
+
+    /**
+     * Toggle window maximize state
+     */
+    toggleMaximize: () => {
+        if (currentWindow && !currentWindow.isDestroyed()) {
+            if (currentWindow.isMaximized()) {
+                currentWindow.unmaximize();
+            } else {
+                currentWindow.maximize();
+            }
+        }
     }
-}
+};
